91dafd361835c0624c3d524ba523151e10a1a8d3
Rifqi Ryan Avila Rakhman <rryanavila9@gmail.com>
2015-05-19 21:49:28 +1000

    add

--- js/addition.js
+++ js/addition.js
@@ -3,6 +3,31 @@ function printIt() {
     window.print();
 }
 
+function save(){
+	var firstName = document.getElementsByName('firstname')[0].value;
+	var lastName = document.getElementsByName('lastname')[0].value;
+	var contact = document.getElementsByName('contact')[0].value;
+	var address = document.getElementsByName('address')[0].value;
+
+	var firstNameShow = document.getElementById('firstName');
+	var lastNameShow = document.getElementById('lastName');
+	var contactShow = document.getElementById('contact');
+	var AddressShow = document.getElementById('Address');
+
+	$("#form").addClass("hidden");
+	$("#show").removeClass("hidden");
+
+	firstNameShow.innerHTML = firstName;
+	lastNameShow.innerHTML = lastName;
+	contactShow.innerHTML = contact;
+	AddressShow.innerHTML = address;
+}
+
+function edit(){
+	$("#show").addClass("hidden");
+	$("#form").removeClass("hidden");
+}
+
 // Can also be used with $(document).ready()
 $(window).load(function() {
   $('.flexslider').flexslider({
